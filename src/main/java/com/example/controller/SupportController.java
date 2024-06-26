daf1ad60bb8685b5bf4a902f5fa69e008d10eb3a
AnveshaShukla <as9334@srmist.edu.in>
2024-06-26 18:38:54 +0530

    First Commit

---
+++ src/main/java/com/example/controller/SupportController.java
@@ -0,0 +1,93 @@
+package com.example.controller;
+
+import com.example.model.EmailDetails;
+import com.example.model.Support;
+import com.example.service.SupportService;
+import com.example.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+// add validations
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/supports")
+public class SupportController {
+	
+    @Autowired
+    private SupportService supportService;
+
+//    private final SupportService supportService;
+//
+//    public SupportController(SupportService supportService) {
+//        this.supportService = supportService;
+//    }
+
+    @GetMapping("/read")
+    public List<Support> read() {
+        return supportService.getAllSupports();
+    }
+
+    // finding by support id
+    @GetMapping("/readOne/{id}")
+    public Support readOne(@PathVariable("id") long id) throws ResourceNotFoundException {
+        return supportService.getSupportById(id);
+    }
+
+    @PostMapping("/add")
+    public Support add(@RequestBody Support support) {
+        return supportService.addSupport(support);
+    }
+
+    @PutMapping("/update/{id}")
+    public void update(@RequestBody Support support, @PathVariable("id") long id) throws ResourceNotFoundException {
+        supportService.updateSupport(id, support);
+    }
+
+    @PatchMapping("/partial-update/{id}")
+    public void partialUpdate(@RequestBody Support support, @PathVariable("id") long id) throws ResourceNotFoundException {
+        supportService.partialUpdateSupport(id, support);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable("id") long id) throws ResourceNotFoundException {
+        supportService.deleteSupport(id);
+    }
+    
+    // find by assigned user id
+    @GetMapping("/open/{userId}")
+    public List<Support> getOpenTickets(@PathVariable("userId") long userId) {
+        return supportService.getOpenTickets(userId);
+    }
+
+ // find by assigned user id
+    @GetMapping("/closed/{userId}")
+    public List<Support> getClosedTickets(@PathVariable("userId") long userId) {
+        return supportService.getClosedTickets(userId);
+    }
+    
+    // add error handling
+    @PostMapping("/send-email")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailDetails emailDetails) {
+        supportService.sendEmail(emailDetails);
+        return ResponseEntity.ok("Email sent successfully");
+    }
+    
+    @GetMapping("/open-cust/{customerId}")
+    public List<Support> getOpenTicketsCustomer(@PathVariable("customerId") long userId) {
+        return supportService.getOpenTicketsCustomer(userId);
+    }
+
+    @GetMapping("/closed-cust/{customerId}")
+    public List<Support> getClosedTicketsCustomer(@PathVariable("customerId") long userId) {
+        return supportService.getClosedTicketsCustomer(userId);
+    }
+
+}
+
+
